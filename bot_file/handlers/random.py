755380585e34eae8d681f87b35d5599ec4651f72
TimurBerdyyev <gorilazanimals@gmail.com>
2024-04-21 18:29:48 +0600

    убрал коменты

--- bot_file/handlers/random.py
+++ bot_file/handlers/random.py
@@ -4,9 +4,7 @@ from ..models import Product
 
 
 async def get_random_product():
-    # Получаем все продукты из базы данных
     all_products = Product.objects.all()
-    # Если есть хотя бы один продукт в базе данных
     if all_products:
         random_product = random.choice(all_products)
         return random_product
